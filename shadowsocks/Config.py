83e5a0c4214b4712360fddc84ad9f9a4f15bb848
aa2568637 <30540935+aa2568637@users.noreply.github.com>
2017-11-11 22:19:30 +0800

    no message

---
+++ shadowsocks/Config.py
@@ -0,0 +1,13 @@
+#Config
+MYSQL_HOST = '176.122.151.136'
+MYSQL_PORT = 3306
+MYSQL_USER = 'root'
+MYSQL_PASS = 'f8cdbfddf1bf870c'
+MYSQL_TABLE= 'user4'
+MYSQL_DB = 'shadowsocks'
+
+MANAGE_PASS = 'ss233333333'
+#if you want manage in other server you should set this value to global ip
+MANAGE_BIND_IP = '127.0.0.1'
+#make sure this port is idle
+MANAGE_PORT = 23333
